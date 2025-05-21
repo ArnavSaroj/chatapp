fb1eefc25fcada967b95bb6a1995035248615569
ARNAVSAROJ <arnavsaroj42@gmail.com>
2025-05-21 23:05:36 +0530

    fixed another error

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,22 +1,23 @@
-import pg from 'pg'
-import dotenv from 'dotenv'
+import pg from 'pg';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db =new  pg.Client({
-    user: process.env.DATABASE_USER,
-    password:process.env.DATABASE_PASSWORD,
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT
-  });
-  db.connect();
-  
-  db.query("select now()", (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("Database connected successfully:😄", result.rows);
-    }
-  });
-export default db;  
+const db = new pg.Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
+});
+
+db.connect();
+
+db.query("select now()", (err, result) => {
+  if (err) {
+    console.error("Database connection error:", err);
+  } else {
+    console.log("Database connected successfully: 😄", result.rows);
+  }
+});
+
+export default db;
